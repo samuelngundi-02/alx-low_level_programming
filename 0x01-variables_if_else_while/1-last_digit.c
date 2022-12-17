a635a6e742434ded90b3ca0002786892a6cc8636
samuelwngundi-02 <samuelwngundi03@gmail.com>
2022-12-17 14:01:24 +0300

    Check if Last digit fix works

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,24 +1,25 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
- * main -Entry point
- *
- * Return: Always 0(Success)
- */
+  * main - checks type of last digit
+  * Return: 0 if Successful
+  */
+/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
+	int Last;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("Last digit of %d is %d ", n, n % 10);
-	if (n % 10 > 5)
-		printf("and is greater than 5\n");
-	else if (n % 10 == 0)
-		printf("and is 0\n");
-	else if (n % 10 < 6 && n % 10 != 0)
-		printf("and is less than 6 and not 0\n");
+	Last = n % 10;
+	if (Last > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, Last);
+	else if (Last == 0)
+		printf("Last digit of %d is %d and is 0\n", n, Last);
+	else if (Last < 6 && Last != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, Last);
+	/* your code goes there */
 	return (0);
 }
